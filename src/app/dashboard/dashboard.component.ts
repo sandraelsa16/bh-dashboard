331585ec570f1b2f6814379b6f6ecec3e1babadb
sandraelsa16 <sandraelsavarughese@gmail.com>
2025-04-15 11:40:45 +0530

    updates added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
-
 import { FooterComponent } from "../footer/footer.component";
+import { PROJECT_DETAILS } from '../projects';
+import { CommonModule } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeaderComponent, FooterComponent],
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-   
+    projects = PROJECT_DETAILS
 }
---
+++ src/app/projects.ts
@@ -0,0 +1,42 @@
+export const PROJECT_DETAILS = [
+    {
+        id: '1',
+        fileName: "Rules Post on Dribble",
+        dotcolor: ' rgb(118, 58, 177)',
+        fileType: "Document",
+        date: "12 Aug 2024",
+        author:"Monry Hasu",
+        expand:"...",
+        icon: "p1.jpg"
+    },
+    {
+        id: '2',
+        fileName: "Login & Sign Up Ui",
+        dotcolor: ' rgb(18, 147, 164)',
+        fileType: "Animation",
+        date: "19 Mar 2024",
+        author:"Alex Madus",
+        expand:"...",
+        icon: "p2.jpg"
+    },
+    {
+        id: '3',
+        fileName: "Nft website Pages",
+        dotcolor:' rgb(231, 96, 38)',
+        fileType: "Image",
+        date: "30 June 2024",
+        author:"Nomru Nalij",
+        expand:"...",
+        icon: "p3.jpg"
+    },
+    {
+        id: '4',
+        fileName: "Square Dashboard",
+        fileType: "Document",
+        dotcolor:' rgb(164, 18, 159)',
+        date: "24 Oct 2024",
+        author:"Willium sen",
+        expand:"...",
+        icon: "p4.jpg"
+    },
+]
